5bb35b567267bb287cc991b3719ac5b0e28a89e1
DiYunTing <dyt99668@163.com>
2020-05-24 12:10:00 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/doven/git/FirstClass.java
@@ -0,0 +1,15 @@
+package com.doven.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git progect");
+        System.out.println("hello git progect");
+        System.out.println("hello git progect");
+        System.out.println("hello git progect");
+        System.out.println("hello git progect");
+        System.out.println("hello git progect");
+        System.out.println("hello git progect");
+        System.out.println("hello git progect");
+        System.out.println("hello git progect");
+    }
+}
